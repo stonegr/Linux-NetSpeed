fa546372fbf1ff17c6d3641060d4208b6a9dad40
stonegr <74632994+stonegr@users.noreply.github.com>
2021-09-08 16:28:05 +0800

    Update tcp1.sh

--- tcp1.sh
+++ tcp1.sh
@@ -61,8 +61,8 @@ installbbrplus(){
 		kernel_version="4.14.129_bbrplus" #fix a bug
 	elif [[ "${release}" == "debian" || "${release}" == "ubuntu" ]]; then
 		mkdir bbrplus && cd bbrplus
-		wget -N --no-check-certificate https://gh.api.99988866.xyz/http://${github}/bbrplus/debian-ubuntu/${bit}/linux-headers-${kernel_version}.deb
-		wget -N --no-check-certificate https://ghproxy.com/http://${github}/bbrplus/debian-ubuntu/${bit}/linux-image-${kernel_version}.deb
+		wget -N --no-check-certificate https://gh.api.99988866.xyz/https://${github}/bbrplus/debian-ubuntu/${bit}/linux-headers-${kernel_version}.deb
+		wget -N --no-check-certificate https://ghproxy.com/https://${github}/bbrplus/debian-ubuntu/${bit}/linux-image-${kernel_version}.deb
 		dpkg -i linux-headers-${kernel_version}.deb
 		dpkg -i linux-image-${kernel_version}.deb
 		cd .. && rm -rf bbrplus
